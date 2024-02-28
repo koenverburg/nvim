8b97c0e4b518cef5ec5c962daac0628cc0cf1f22
Koen Verburg <creativekoen@gmail.com>
2024-02-28 21:37:17 +0100

    plugin(add) git

---
+++ lua/plugins/git.lua
@@ -0,0 +1,43 @@
+require("globals")
+
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    enabled = Is_enabled("git"),
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        untracked = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+      },
+      numhl = true,
+      linehl = false,
+    },
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
+    end,
+  },
+  {
+    "TimUntersberger/neogit",
+    enabled = Is_enabled("git"),
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>" },
+    },
+    config = function()
+      require("neogit").setup({
+        integrations = {
+          diffview = true,
+        },
+      })
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    enabled = Is_enabled("git")
+  },
+}
