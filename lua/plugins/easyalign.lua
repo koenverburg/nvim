3a72988426cdb7f71863f8c248d298905c1e6db4
Koen Verburg <creativekoen@gmail.com>
2024-03-29 08:11:58 +0100

    plugin(add) easy align

--- lua/plugins/easyalign.lua
+++ lua/plugins/easyalign.lua
@@ -1,8 +1,10 @@
 require("globals")
--- local p = require("custom.lines.provider")
 
 return {
   "junegunn/vim-easy-align",
   enabled = Is_enabled("easy-align"),
   lazy = false,
+  keys = {
+    { "ga", "<Plug>(EasyAlign)", desc = "EaslyAgain" },
+  },
 }
